3c02ec174fa674106fbc97f52d416653781fd24a
erazor <52050311+erazoor@users.noreply.github.com>
2023-11-28 14:38:04 +0100

    chore: added jest config

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	clearMocks: true,
+	coverageDirectory: 'coverage',
+	moduleFileExtensions: ['js', 'node'],
+	testEnvironment: 'node',
+	testMatch: ['**/tests/**/*.test.js', '**/__tests__/**/*.test.js'],
+	testPathIgnorePatterns: ['/node_modules/'],
+	testURL: 'http://localhost',
+	watchman: true,
+};
