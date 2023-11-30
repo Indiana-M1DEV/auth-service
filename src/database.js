257f096b37caaa2bd53af4035fac08e168d26e5e
erazor <52050311+erazoor@users.noreply.github.com>
2023-11-30 11:22:20 +0100

    chore: removed conn string & changed vars

--- src/database.js
+++ src/database.js
@@ -5,7 +5,6 @@ mongoose.set('strictQuery', false);
 const connect = () => {
 	mongoose
 		.connect(
-			// 'mongodb://indiana:1nd14n4-D3v@indiana-auth-db:27017/?authMechanism=SCRAM-SHA-256&authSource=admin'
 			`mongodb://${process.env.DATABASE_LOG}:${process.env.DATABASE_PASS}@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/?authMechanism=${process.env.AUTH_MECANISM}&authSource=${process.env.AUTH_SOURCE}`
 		)
 		.then(() => console.log('Connected to MongoDB'))
